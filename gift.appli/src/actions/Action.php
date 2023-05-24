efcf9aea05cc86495ce6ad77c9deb34e0ed5ca9c
Nael Guenfoudi <guenfoud4u@iutnc-503-01.iutnc.site.univ-lorraine.fr>
2023-05-24 16:04:39 +0200

    mise en place du projet jusqu'au TD4

--- gift.appli/src/actions/Action.php
+++ gift.appli/src/actions/Action.php
@@ -8,18 +8,6 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 abstract class Action
 {
 
-<<<<<<< HEAD
     public abstract function __invoke($request, Response $response,$args):Response;
     
-=======
-    public function __invoke(Request $request, Response $response, $args): Response
-    {
-        $html = $this->run($request, $response, $args);
-        $response->getBody()->write($html);
-        return $response;
-    }
-
-    abstract public function run(Request $request, Response $response, $args): string;
-
->>>>>>> parent of a1c5989 (V4 du TD3 - Q3 Fonctionelle)
 }
--- gift.appli/src/actions/CategoryByIdAction.php
+++ gift.appli/src/actions/CategoryByIdAction.php
@@ -2,54 +2,20 @@
 
 namespace gift\app\actions;
 
-<<<<<<< HEAD
 use Psr\Http\Message\ResponseInterface as Response;
 use gift\app\models\Categorie;
 use Slim\Views\Twig;
 
-=======
-use gift\app\models\Categorie;
-use gift\app\services\CategoriesService;
-use gift\app\services\PrestationsService;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
->>>>>>> parent of a1c5989 (V4 du TD3 - Q3 Fonctionelle)
 
 class CategoryByIdAction extends Action
 {
 
     public function __invoke($request, Response $response, $args): Response
     {
-<<<<<<< HEAD
         $idOfCategorie = $args['id'];
         $categorie = Categorie::with('prestations')->findOrFail($idOfCategorie);
         $view = Twig::fromRequest($request);
         
         return $view->render($response, 'categorie.twig', ['categorie' => $categorie]);
-=======
-        $categorie = (new CategoriesService)->getCategorieById($args['id']);
-        $html = <<<HTML
-            <html lang="fr">
-            <head>
-                <title>Catégorie {$categorie["libelle"]}</title>
-            </head>
-            <body>
-                <h1>Catégorie {$categorie["libelle"]}</h1>
-                <ul>
-        HTML;
-        foreach (
-            (new PrestationsService())->getPrestationsByCategorie($categorie["id"]) as $prestation
-        ) {
-            $html .= <<<HTML
-                    <li><a href="/prestation?id={$prestation["id"]}">{$prestation["libelle"]}</a></li>
-            HTML;
-        }
-        $html .= <<<HTML
-                </ul>
-            </body>
-            </html>
-        HTML;
-        return $html;
->>>>>>> parent of a1c5989 (V4 du TD3 - Q3 Fonctionelle)
     }
 }
