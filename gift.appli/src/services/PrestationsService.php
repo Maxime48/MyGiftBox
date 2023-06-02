45fef24e3a94b213ee9ed32ee714f3a8e073eebc
ismael <ismael.kone4@etu.univ-lorraine.fr>
2023-06-02 11:38:41 +0200

    etape 2: createCategorie

--- gift.appli/src/services/PrestationsService.php
+++ gift.appli/src/services/PrestationsService.php
@@ -5,6 +5,7 @@ namespace gift\app\services;
 use gift\app\models\Categorie;
 use gift\app\models\Prestation;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use gift\app\services\utils\CsrfService;
 
 class PrestationsService
 {
@@ -25,6 +26,36 @@ class PrestationsService
         } catch (ModelNotFoundException $e) {
             throw new PrestationNotFoundException("Catégorie inconnue ou sans prestation: ". $categ_id);
         }
+    }
+
+    public function createCategorie($data) {
+        // Vérifie le token CSRF
+        CsrfService::checkToken($data['csrf_token']);
+
+        // Vérifie la présence des données nécessaires
+        if (!isset($data['libelle']) || !isset($data['description'])) {
+            throw new \Exception("Données manquantes pour créer la catégorie");
+        }
+
+        // Filtre les données pour se prémunir des injections XSS
+        $libelle = filter_var($data['libelle'], FILTER_UNSAFE_RAW);
+        $description = filter_var($data['description'], FILTER_UNSAFE_RAW);
+
+        // Vérifie si les données filtrées sont identiques aux données originales
+        if ($libelle !== $data['libelle'] || $description !== $data['description']) {
+            throw new \Exception("Données suspectes pour créer la catégorie");
+        }
+
+        // Crée une nouvelle instance de la catégorie
+        $categorie = new Categorie();
+        $categorie->libelle = $libelle;
+        $categorie->description = $description;
+
+        // Enregistre la catégorie dans la base de données
+        $categorie->save();
+
+        // Retourne la catégorie créée
+        return $categorie;
     }
 
 }
