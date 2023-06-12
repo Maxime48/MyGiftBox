cd4d3699673f8ee8d011706c8501f443eacb1cdc
ismael <ismael.kone4@etu.univ-lorraine.fr>
2023-06-12 11:21:09 +0200

    CreateCatgorieAction

--- gift.appli/src/actions/CreateCatgorieAction.php
+++ gift.appli/src/actions/CreateCatgorieAction.php
@@ -5,6 +5,7 @@ namespace gift\app\actions;
 use gift\app\services\CategoriesService;
 use Psr\Http\Message\ResponseInterface as Response;
 use gift\app\models\Categorie;
+use gift\app\services\utils\CsrfService;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
 
@@ -13,14 +14,12 @@ class CreateCatgorieAction extends Action
 
     public function __invoke(Request $request, Response $response, $args): Response
     {
-        $categorie = new Categorie();
-        $categorie->nom = $request->getParsedBody()['nom'];
-        $categorie->description = $request->getParsedBody()['description'];
-        $categorie->save();
-        return Twig::fromRequest($request)->render($response, 'categorie.twig',
-            ['categorie' =>
-                CategoriesService::getCategorieById($categorie->id)
-            ]
-        );
+        $csrfToken = CsrfService::generateToken();
+
+        return Twig::fromRequest($request)->render($response, 'create_categorie.twig', [
+            'csrf_token' => $csrfToken
+        ]);
     }
 }
+
+// Path: gift.appli/src/actions/CreateCadeauAction.php
